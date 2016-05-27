278aec1f8fcfabf770e8d1f22023f16a31c68c51
DwViVi <13716373643@163.com>
2016-05-27 11:56:15 +0800

    双击后退键，退出程序

--- app/src/main/java/com/dwvivi/superdemo2/MainActivity.java
+++ app/src/main/java/com/dwvivi/superdemo2/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dwvivi.superdemo2;
 
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private long lastTime;
+    @Override
+    public void onBackPressed() {
+       long ct= System.currentTimeMillis();
+        if(ct-lastTime>2000){
+            Toast.makeText(MainActivity.this, "再次点击后退键退出", Toast.LENGTH_SHORT).show();
+            lastTime=ct;
+        }else {
+            super.onBackPressed();
+        }
     }
 }
